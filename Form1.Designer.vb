747d77abc124d43a1ff659a73ad02b3f34be3ca1
Pedro Arruda <pedro.arruda@outlook.com.br>
2014-09-13 22:57:37 -0300

    SimpleFactoryFinancials0.1
    You have to inform only factory costs, distributor valor, government
    taxes and shows to you the final costs.

---
+++ Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LblCustoFabrica = New System.Windows.Forms.Label()
+        Me.LblValorDistribuidor = New System.Windows.Forms.Label()
+        Me.LblValorImpostos = New System.Windows.Forms.Label()
+        Me.LblCustoFinal = New System.Windows.Forms.Label()
+        Me.BtnCalcular = New System.Windows.Forms.Button()
+        Me.BtnLimpar = New System.Windows.Forms.Button()
+        Me.BtnSair = New System.Windows.Forms.Button()
+        Me.TxtCustoFabrica = New System.Windows.Forms.TextBox()
+        Me.TxtValorDistribuidor = New System.Windows.Forms.TextBox()
+        Me.TxtValorImpostos = New System.Windows.Forms.TextBox()
+        Me.TxtCustoFinal = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'LblCustoFabrica
+        '
+        Me.LblCustoFabrica.AutoSize = True
+        Me.LblCustoFabrica.Location = New System.Drawing.Point(16, 13)
+        Me.LblCustoFabrica.Name = "LblCustoFabrica"
+        Me.LblCustoFabrica.Size = New System.Drawing.Size(90, 13)
+        Me.LblCustoFabrica.TabIndex = 0
+        Me.LblCustoFabrica.Text = "Custo de Fábrica:"
+        '
+        'LblValorDistribuidor
+        '
+        Me.LblValorDistribuidor.AutoSize = True
+        Me.LblValorDistribuidor.Location = New System.Drawing.Point(16, 48)
+        Me.LblValorDistribuidor.Name = "LblValorDistribuidor"
+        Me.LblValorDistribuidor.Size = New System.Drawing.Size(89, 13)
+        Me.LblValorDistribuidor.TabIndex = 2
+        Me.LblValorDistribuidor.Text = "Valor Distribuidor:"
+        '
+        'LblValorImpostos
+        '
+        Me.LblValorImpostos.AutoSize = True
+        Me.LblValorImpostos.Location = New System.Drawing.Point(16, 90)
+        Me.LblValorImpostos.Name = "LblValorImpostos"
+        Me.LblValorImpostos.Size = New System.Drawing.Size(76, 13)
+        Me.LblValorImpostos.TabIndex = 4
+        Me.LblValorImpostos.Text = "Valor Impostos"
+        '
+        'LblCustoFinal
+        '
+        Me.LblCustoFinal.AutoSize = True
+        Me.LblCustoFinal.Location = New System.Drawing.Point(16, 131)
+        Me.LblCustoFinal.Name = "LblCustoFinal"
+        Me.LblCustoFinal.Size = New System.Drawing.Size(62, 13)
+        Me.LblCustoFinal.TabIndex = 6
+        Me.LblCustoFinal.Text = "Custo Final:"
+        '
+        'BtnCalcular
+        '
+        Me.BtnCalcular.Location = New System.Drawing.Point(16, 169)
+        Me.BtnCalcular.Name = "BtnCalcular"
+        Me.BtnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.BtnCalcular.TabIndex = 8
+        Me.BtnCalcular.Text = "Calcular"
+        Me.BtnCalcular.UseVisualStyleBackColor = True
+        '
+        'BtnLimpar
+        '
+        Me.BtnLimpar.Location = New System.Drawing.Point(97, 169)
+        Me.BtnLimpar.Name = "BtnLimpar"
+        Me.BtnLimpar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnLimpar.TabIndex = 9
+        Me.BtnLimpar.Text = "Limpar"
+        Me.BtnLimpar.UseVisualStyleBackColor = True
+        '
+        'BtnSair
+        '
+        Me.BtnSair.Location = New System.Drawing.Point(178, 169)
+        Me.BtnSair.Name = "BtnSair"
+        Me.BtnSair.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSair.TabIndex = 10
+        Me.BtnSair.Text = "Sair"
+        Me.BtnSair.UseVisualStyleBackColor = True
+        '
+        'TxtCustoFabrica
+        '
+        Me.TxtCustoFabrica.Location = New System.Drawing.Point(112, 10)
+        Me.TxtCustoFabrica.Name = "TxtCustoFabrica"
+        Me.TxtCustoFabrica.Size = New System.Drawing.Size(141, 20)
+        Me.TxtCustoFabrica.TabIndex = 1
+        Me.TxtCustoFabrica.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TxtValorDistribuidor
+        '
+        Me.TxtValorDistribuidor.Enabled = False
+        Me.TxtValorDistribuidor.Location = New System.Drawing.Point(112, 45)
+        Me.TxtValorDistribuidor.Name = "TxtValorDistribuidor"
+        Me.TxtValorDistribuidor.Size = New System.Drawing.Size(141, 20)
+        Me.TxtValorDistribuidor.TabIndex = 3
+        Me.TxtValorDistribuidor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TxtValorImpostos
+        '
+        Me.TxtValorImpostos.Enabled = False
+        Me.TxtValorImpostos.Location = New System.Drawing.Point(112, 87)
+        Me.TxtValorImpostos.Name = "TxtValorImpostos"
+        Me.TxtValorImpostos.Size = New System.Drawing.Size(141, 20)
+        Me.TxtValorImpostos.TabIndex = 5
+        Me.TxtValorImpostos.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'TxtCustoFinal
+        '
+        Me.TxtCustoFinal.Location = New System.Drawing.Point(112, 128)
+        Me.TxtCustoFinal.Name = "TxtCustoFinal"
+        Me.TxtCustoFinal.Size = New System.Drawing.Size(141, 20)
+        Me.TxtCustoFinal.TabIndex = 7
+        Me.TxtCustoFinal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(269, 207)
+        Me.Controls.Add(Me.TxtCustoFinal)
+        Me.Controls.Add(Me.TxtValorImpostos)
+        Me.Controls.Add(Me.TxtValorDistribuidor)
+        Me.Controls.Add(Me.TxtCustoFabrica)
+        Me.Controls.Add(Me.BtnSair)
+        Me.Controls.Add(Me.BtnLimpar)
+        Me.Controls.Add(Me.BtnCalcular)
+        Me.Controls.Add(Me.LblCustoFinal)
+        Me.Controls.Add(Me.LblValorImpostos)
+        Me.Controls.Add(Me.LblValorDistribuidor)
+        Me.Controls.Add(Me.LblCustoFabrica)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Custo Final de um Carro"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LblCustoFabrica As System.Windows.Forms.Label
+    Friend WithEvents LblValorDistribuidor As System.Windows.Forms.Label
+    Friend WithEvents LblValorImpostos As System.Windows.Forms.Label
+    Friend WithEvents LblCustoFinal As System.Windows.Forms.Label
+    Friend WithEvents BtnCalcular As System.Windows.Forms.Button
+    Friend WithEvents BtnLimpar As System.Windows.Forms.Button
+    Friend WithEvents BtnSair As System.Windows.Forms.Button
+    Friend WithEvents TxtCustoFabrica As System.Windows.Forms.TextBox
+    Friend WithEvents TxtValorDistribuidor As System.Windows.Forms.TextBox
+    Friend WithEvents TxtValorImpostos As System.Windows.Forms.TextBox
+    Friend WithEvents TxtCustoFinal As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub BtnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnCalcular.Click
+        'declara variáveis
+        Dim custoFabrica As Double
+        Dim valorDistribuidor As Double
+        Dim valorImpostos As Double
+        Dim custoFinal As Double
+        'lê custo de fábrica
+        custoFabrica = Convert.ToDouble(TxtCustoFabrica.Text)
+        'calcula valor distribuidor
+        valorDistribuidor = 0.28 * custoFabrica
+        'calcula valor dos impostos
+        valorImpostos = 0.45 * custoFabrica
+        'calcula o custo final
+        custoFinal = custoFabrica + valorDistribuidor + valorImpostos
+        'exibe o custo final
+        TxtValorDistribuidor.Text = valorDistribuidor.ToString("F")
+        TxtValorImpostos.Text = valorImpostos.ToString("F")
+        TxtCustoFinal.Text = custoFinal.ToString("F")
+    End Sub
+
+    Private Sub BtnLimpar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnLimpar.Click
+        'limpa as caixas de texto
+        TxtCustoFabrica.Clear()
+        TxtValorDistribuidor.Clear()
+        TxtValorImpostos.Clear()
+        TxtCustoFinal.Clear()
+        'foca na caixa de texto custo fábrica
+        TxtCustoFabrica.Focus()
+    End Sub
+
+    Private Sub BtnSair_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSair.Click
+        If (MessageBox.Show(Me,
+                            "Sair?",
+                            "Atenção",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question,
+            MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.Yes) Then
+            End
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If (MessageBox.Show(Me,
+                            "Fechar?",
+                            "Atenção",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question,
+            MessageBoxDefaultButton.Button2) = Windows.Forms.DialogResult.No) Then
+            e.Cancel = True
+        End If
+    End Sub
+End Class
